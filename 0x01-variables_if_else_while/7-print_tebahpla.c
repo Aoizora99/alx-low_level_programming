41497b92a0b38737b6b591f6ef39c48c44b9add5
Aoizora99 <you@example.com>
2023-03-28 03:28:18 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main -  prints all single digit numbers of base 10 starting from 0.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char i;
+for (i = 'z'; i <= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
